477639648a6531480364ed6660acec243d0a84cb
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-24 11:15:21 +0530

    FilterBar - added div and select method.

--- task-manager-dashboard/src/components/FilterBar.jsx
+++ task-manager-dashboard/src/components/FilterBar.jsx
@@ -6,6 +6,15 @@ const FilterBar = () => {
     const setFilter = useTaskStore((state) => state.setFilter);
 
     return (
-    
+    <div className="flex justify-end mb-4">
+        <select
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            className="px-4 py-2 border border-gray-300 rounded-md bg-white focus:outline-none focus:rind-2 focus:ring-blue-500"
+        >
+            <option value="all">All Task</option>
+
+         </select>
+    </div>
     )
 }
