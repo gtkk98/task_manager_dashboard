1bfbb3779074fe4a96b06848de284f72b0741c1f
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-24 11:29:41 +0530

    TaskItems - added div to flex and design checkbox input

--- task-manager-dashboard/src/components/TaskItem.jsx
+++ task-manager-dashboard/src/components/TaskItem.jsx
@@ -5,6 +5,11 @@ const TaskItem = ({ task }) => {
     const toggleTaskCompletion = useTaskStore((state) => state.toggleTaskCompletion);
 
     return (
-        
+        <div className="flex items-center justify-between p-4 mb-2 bg-white border border-gray-200 rounded-md shadow-md hover:shadow-md transition-shadow">
+            <div className="flex items-center gap-4">
+
+
+            </div>
+        </div>
     )
 }
