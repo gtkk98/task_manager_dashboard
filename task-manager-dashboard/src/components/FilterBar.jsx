625d14e8b6180fcfa80fe3e4dcd61e2a60aaace8
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-04-24 11:18:22 +0530

    FilterBar.jsx successfully added.

--- task-manager-dashboard/src/components/FilterBar.jsx
+++ task-manager-dashboard/src/components/FilterBar.jsx
@@ -13,8 +13,11 @@ const FilterBar = () => {
             className="px-4 py-2 border border-gray-300 rounded-md bg-white focus:outline-none focus:rind-2 focus:ring-blue-500"
         >
             <option value="all">All Task</option>
-
+            <option value="completed">Completed</option>
+            <option value="incomplete">Incompleted</option>
          </select>
     </div>
-    )
-}
+    );
+};
+
+export default FilterBar;
